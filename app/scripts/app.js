33d1af9f74c90c7225bfd332ff8c64087b02f0b7
4enki <mail@4enki.ru>
2016-09-19 02:15:50 +0300

    развели чуть-чуть бардака, закончена разметка всех блоков, начат обработчик форм

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -23,4 +23,87 @@ $(document).ready(function() {
     return false;
   });
 
+  $('._zip').inputmask("99999", {"placeholder": "_____"});
+
+
+  function clear_form() {
+    $("input[type='text']").val("");
+  };
+
+  var emailCount = 0;
+
+  function email_validate(x) {
+    var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
+    if(pattern.test(x.val())){
+      x.removeClass('error');
+      emailCount=0;
+    }
+    else if (x.val() == 0) {
+      x.removeClass('error');
+      emailCount=0;
+    }
+    else {
+      x.addClass('error');
+      emailCount=1;
+    }
+    console.log(emailCount);
+  };
+
+  $('input[name="email"]').blur(function() {
+    email_validate($(this));
+  });
+
+  $('input[name="email"]').keypress(function(e){
+    if(e.keyCode==13){
+      email_validate($(this));
+    }
+  });
+
+  $('input[name="phone"]').blur(function() {
+    var phone =  $(this).val();
+    var index =  phone.indexOf('_');
+    if(phone.length >= 6 && index == -1) {
+      $(this).removeClass('error');
+    }
+    else if (phone == 0) {
+      $(this).removeClass('error');
+    }
+    else {
+      $(this).addClass('error');
+    }
+  });
+
+  $('._fs').submit(function(){
+  var name = $(this).find('input[name="fname"]').val();
+  var phone =  $(this).find('input[name="phormail"]').val();
+  var index =  phone.indexOf('_');
+  var indexstring =  phone.indexOf('е');
+  var companyName = $(this).find('input[name="zipf"]').val();
+  var formDate = $(this).find('input[name="zipt"]').val();
+  var formName = $(this).find('input[name="hidden"]').val();
+  if(phone.length >= 6 && index == -1 && emailCount == 0 && indexstring == -1) {
+    $.ajax({
+      type: "POST",
+      url: "mail.php",
+      data:{
+        "name":name,
+        "phone":phone,
+        "companyName":companyName,
+        "formDate":formDate,
+        "formName":formName
+      },
+      success: function() {
+        clear_form();
+        $('.modal').fadeOut();
+        $('.modalThanks').fadeIn();
+        $('input[name="name"], input[name="phone"]').removeClass('error');
+      }
+    });
+  }
+  else {
+    $(this).find('input[name="phone"]').addClass('error');
+  }
+  return false;
+  });
+
 });
