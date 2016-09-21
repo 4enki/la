4051383f377ad449464ccc721c3fc3d36d7022f8
4enki <mail@4enki.ru>
2016-09-21 17:21:59 +0300

    порядок в скриптах, фиксы стилей мелкие, новые фоновые картинки оптимизированные

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -34,53 +34,46 @@ $(document).ready(function() {
   $('._zip').inputmask("99999", {"placeholder": "_____"});
   // /маска для полей zip-кодов
 
-// Cache selectors
-var lastId,
+  // шпионское меню со скроллом
+  var lastId,
     topMenu = $("#top-menu"),
     topMenuHeight = topMenu.outerHeight()+0,
-    // All list items
     menuItems = topMenu.find("a"),
-    // Anchors corresponding to menu items
     scrollItems = menuItems.map(function(){
       var item = $($(this).attr("href"));
       if (item.length) { return item; }
     });
-
-// Bind click handler to menu items
-// so we can get a fancy scroll animation
-menuItems.click(function(e){
-  var href = $(this).attr("href"),
+  menuItems.click(function(e){
+    var href = $(this).attr("href"),
       offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
-  $('html, body').stop().animate({
+    $('html, body').stop().animate({
       scrollTop: offsetTop
-  }, 300);
-  e.preventDefault();
-});
-
-// Bind to scroll
-$(window).scroll(function(){
-   // Get container scroll position
+    }, 300);
+    e.preventDefault();
+  });
+  $(window).scroll(function(){
    var fromTop = $(this).scrollTop()+topMenuHeight;
-
-   // Get id of current scroll item
    var cur = scrollItems.map(function(){
      if ($(this).offset().top < fromTop)
        return this;
    });
-   // Get the id of the current element
    cur = cur[cur.length-1];
    var id = cur && cur.length ? cur[0].id : "";
-
    if (lastId !== id) {
-       lastId = id;
-       // Set/remove active class
-       menuItems
-         .parent().removeClass("active")
-         .end().filter("[href=#"+id+"]").parent().addClass("active");
+     lastId = id;
+     menuItems
+       .parent().removeClass("active")
+       .end().filter("[href=#"+id+"]").parent().addClass("active");
    }
-});
+  });
+  // /шпионское меню со скроллом
 
-
+  	$('._go-to').on('click', function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 310);
+	});
 
 
   function clear_form() {
